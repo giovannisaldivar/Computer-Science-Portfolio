ef3e3e0a090e68d7de637d0cd3be8f9b4ec015eb
giovannisaldivar <gsaldivar.ep@mysummitps.org>
2016-05-12 17:12:29 +0000

    update

--- 777.js
+++ 777.js
@@ -1,10 +1,12 @@
  var count = 0;
-
+//function to wtite the messages
  function write_message($message) {
      document.getElementById('messages').innerHTML = $message;
  }
 
+//picks three random numbers when you click spin
  function refresh() {
+  //changes background color to black after you click spin
   document.body.style.backgroundColor = "black";
      if (count > 0) {
          refreshCounts(-1);
@@ -15,29 +17,37 @@
          document.getElementById('first').innerHTML = one;
          document.getElementById('second').innerHTML = two;
          document.getElementById('third').innerHTML = three;
+         //checks if all three numbers are equal
          if (one == two && two == three && three == one) {
+          //writes message "winner!"
              write_message('Winner!');
+             //adds 25 credits 
                 count = count + 25
                 document.getElementById("p2").innerHTML = count;
+                //changes background color to red
                 document.body.style.backgroundColor = "red";
          }
+         //checks if the first number equals the second number
          else if(one == two){
+          //add 10 credits
           count = count + 10
           document.getElementById("p2").innerHTML = count;
          }
          
      
-         
+         //checks if the last two numbers are equal
          else if(two == three){
+          //adds 10 credits
           count = count + 10
           document.getElementById("p2").innerHTML = count;
          }
       
-         
+         //if none of the numbers equal eachother it sends message "try again!"
          else {
              write_message('Try again!');
          }
      }
+     //says "insert credits!" if there are no credits
      else {
          write_message('Insert credits!');
      }
